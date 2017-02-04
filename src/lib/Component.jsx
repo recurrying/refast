42c28c7c0572906b5d74617b3d2e68b71cb5d2e7
wenzhao.fw <wenzhao.fw@alibaba-inc.com>
2017-02-04 19:27:01 +0800

    fix bug

--- src/lib/Component.jsx
+++ src/lib/Component.jsx
@@ -5,7 +5,7 @@
  */
 import React, { PropTypes } from 'react';
 import isArray from 'isarray';
-import deepcopy from 'deepequal';
+import deepcopy from 'deepcopy';
 import assign from 'object-assign';
 import { context } from './utils';
 
