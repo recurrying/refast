19eb1d46cc5aaae2c0f0e1acdb0f5abc2ca18b7c
wenzhao.fw <wenzhao.fw@alibaba-inc.com>
2017-10-21 18:14:36 +0800

    将 LogicRender 移出主库，单独发包 refast-logic-render

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import LogicRender from './lib/LogicRender';
 import Component from './lib/Component';
 import PureComponent from './lib/PureComponent';
 import { use } from './lib/utils';
@@ -6,7 +5,6 @@ import { use } from './lib/utils';
 export default {
   Component,
   PureComponent,
-  LogicRender,
   use,
   setup: use,
 };
